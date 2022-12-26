9f2a6858952a7924489623c2111dd52f4b15e55a
ifymatics <ifymatics2004@gmail.com>
2022-12-26 13:41:24 +0100

    First commit on fyconnect frontend

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import Content from "../content/Content";
 
 import LeftBar from "../leftBar/LeftBar";
 import NavBar from "../navBar/NavBar";
@@ -12,8 +11,8 @@ const Home = () => {
       <NavBar />
       <div className="Home">
         <LeftBar />
-        <Content />
-        {/* <Outlet></Outlet> */}
+        {/* <Content /> */}
+        <Outlet></Outlet>
         <RightBar />
       </div>
     </div>
